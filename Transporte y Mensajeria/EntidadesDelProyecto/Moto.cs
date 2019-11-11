a554c0087552f7dfbf0b7e5977ad60de3491daa7
AsenBoris <borisyasen@gmail.com>
2019-11-10 21:23:42 -0300

    Arreglando errores de compilacion 1

---
+++ Transporte y Mensajeria/EntidadesDelProyecto/Moto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesDelProyecto
+{
+    public class Moto : Vehiculo
+    {
+        //Atributos
+        int cilindrada;
+
+        //Setters and Getters
+        public int Cilindrada { get => cilindrada; set => cilindrada = value; }
+
+        //Constructor
+        protected Moto(string descripcion, DateTime fechaCompra, double precioCompra, int cilindrada) : base(descripcion, fechaCompra, precioCompra)
+        {
+            this.cilindrada = cilindrada;
+        }
+
+        //Otros Metodos
+        public override double CalcularPrecio(double precioNeto, double peso)
+        {
+            if (peso > 300)
+            {
+                return precioNeto * ((Aumento / 100) + 1);
+            }
+            else
+            {
+                return precioNeto;
+            }
+        }
+
+
+
+
+    }
+
+}
