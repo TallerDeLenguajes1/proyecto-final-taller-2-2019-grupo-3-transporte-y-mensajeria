24bf76122aaa0825575a1816a9ff51d83eceb66d
AsenBoris <borisyasen@gmail.com>
2019-11-10 21:40:30 -0300

    Punto de partida para agregar clases de acceso a datos

---
+++ Transporte y Mensajeria/EntidadesDelProyecto/Furgoneta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesDelProyecto
+{
+    public class Furgoneta : Vehiculo
+    {
+        //Atributos
+        double capacidadCarga;
+
+        //Setters and Getters
+        public double CapacidadCarga { get => capacidadCarga; set => capacidadCarga = value; }
+
+        //Constructor
+        protected Furgoneta(string descripcion, DateTime fechaCompra, double precioCompra, double capacidadCarga) : base(descripcion, fechaCompra, precioCompra)
+        {
+            this.capacidadCarga = capacidadCarga;
+        }
+
+        //Otros Metodos
+        public override double CalcularPrecio(double precioNeto, double volumen)
+        {
+            if (volumen > 1)
+            {
+                return precioNeto * ((Aumento / 100) + 1);
+            }
+            else
+            {
+                return precioNeto;
+            }
+        }
+
+
+
+
+    }
+
+}
---
+++ Transporte y Mensajeria/EntidadesDelProyecto/Vehiculo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesDelProyecto
+{
+    public abstract class Vehiculo
+    {
+        //Atributos
+        int idVehiculo;
+        string descripcion;
+        DateTime fechaCompra;
+        double precioCompra;
+        static double aumento;
+
+        //Setters and Getters
+        public int IdVehiculo { get => idVehiculo; set => idVehiculo = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public DateTime FechaCompra { get => fechaCompra; set => fechaCompra = value; }
+        public double PrecioCompra { get => precioCompra; set => precioCompra = value; }
+        public static double Aumento { get => aumento; set => aumento = value; }
+
+        //Constructor
+        protected Vehiculo(string descripcion, DateTime fechaCompra, double precioCompra)
+        {
+            this.descripcion = descripcion;
+            this.fechaCompra = fechaCompra;
+            this.precioCompra = precioCompra;
+        }
+
+        //Otros Metodos
+        public virtual double CalcularPrecio(double precioMercancia, double unidad) { return 0; }
+
+    }
+}
